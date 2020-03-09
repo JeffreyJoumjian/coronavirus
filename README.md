40fdcf2ae70c5bf623bd7f2d391c847cc54ad38b
skylerdj <jeffreyjoumjian@hotmail.com>
2020-03-10 01:34:18 +0200

    China causing issues for newCases

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # coronavirus
-A small Node JS program to quickly check coronavirus statistics from https://www.worldometers.info/coronavirus/ .
+
+A small Node JS program to quickly check coronavirus statistics from https://www.worldometers.info/coronavirus/
+
+# Installation
+
+`clone https://github.com/JeffreyJoumjian/coronavirus.git`
+
+<span style="color:rgb(200,0,0);"> • Make sure you have Chrome installed on you computer. </span>
+<span style="color:rgb(200,0,0);"> • Make sure you have [NodeJS & npm](https://nodejs.org/en/) installed on your computer. </span>
+
+I have included the node_modules folder. It's always recommended to run `npm install` to make sure everything is up to date.
+
+# Setup
+
+1 - Open Chrome and type `chrome://version` in the search bar.
+
+2 - Copy the Executable Path and replace the variable:
+`const executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'`
+
+3 - It is recommended to create a bash/zsh alias to run the program easily with 1 command.
+
+# Run
+
+`node index.js <optional-country>`
+
+1 - `node index.js`
+
+```
+–> node index.js
+connected to https://www.worldometers.info/coronavirus/
+┌───────────┬───────────┐
+│  (index)  │  Values   │
+├───────────┼───────────┤
+│   cases   │ '114,285' │
+│  deaths   │  '4,009'  │
+│ deathRate │  '3.51%'  │
+└───────────┴───────────┘
+```
+
+2 - `node index.js <country-name>`
+
+```
+–> node index.js france
+connected to https://www.worldometers.info/coronavirus/
+┌───────────┬───────────┐
+│  (index)  │  Values   │
+├───────────┼───────────┤
+│   cases   │ '114,285' │
+│  deaths   │  '4,009'  │
+│ deathRate │  '3.51%'  │
+└───────────┴───────────┘
+┌────────────┬──────────┐
+│  (index)   │  Values  │
+├────────────┼──────────┤
+│  country   │ 'France' │
+│ totalCases │ '1,412'  │
+│  newCases  │   203    │
+│   deaths   │    30    │
+│ newDeaths  │    11    │
+│ deathRate  │ '2.12%'  │
+│ recovered  │   '12'   │
+│   active   │ '1,370'  │
+│  critical  │    66    │
+└────────────┴──────────┘
+```
---
+++ index.js
@@ -0,0 +1,79 @@
+const puppeteer = require('puppeteer-core');
+
+// REPLACE this by opening Chrome and typing chrome://version and then copying the Execution Path.
+const executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+async function getCountryDetails(country) {
+	// source
+	const url = 'https://www.worldometers.info/coronavirus/';
+
+	// if a country you provide a country name, you'll get another table about that country's status
+	// China is returning a false for new cases => help is appreciated
+	const countryName = process.argv[2] ? process.argv[2].toLowerCase().trim() : undefined;
+
+	const browser = await puppeteer.launch({ executablePath });
+
+	let page;
+	try {
+		page = await browser.newPage();
+
+		await page.goto(url, { 'waitUntil': 'domcontentloaded' });
+
+		console.log(`connected to ${url}`);
+
+		const result = await page.evaluate((countryName) => {
+
+			// general numbers about the virus
+			const numbers = [...document.querySelectorAll('.maincounter-number > span')];
+			const cases = parseFloat(numbers[0].innerText.split(",").join(""));
+			const deaths = parseFloat(numbers[1].innerText.split(",").join(""));
+			let country;
+
+			// if country is provided => get country row
+			if (countryName) {
+				console.log(countryName)
+				country = [...document.querySelectorAll('tbody tr td')].filter((e) => {
+					return e.innerText.toLowerCase() === countryName;
+				})[0].parentNode.children;
+
+				function getNumber(element) {
+					return parseInt(element.innerText.trim().split(",").join("")) ? parseInt(element.innerText.trim().split(",").join("")) : 0;
+				}
+
+				function getInnerText(element) {
+					return element.innerText ? element.innerText : 0;
+				}
+			}
+
+			return {
+				cases: numbers[0].innerText,
+				deaths: numbers[1].innerText,
+				deathRate: `${parseFloat((deaths * 100 / cases).toFixed(2))}%`,
+
+				// if country is provided => add country property with details
+				country: country ? {
+					country: getInnerText(country[0]),
+					totalCases: getInnerText(country[1]),
+					newCases: getNumber(country[2]),
+					deaths: getNumber(country[3]),
+					newDeaths: getNumber(country[4]),
+					deathRate: `${parseFloat((getNumber(country[3]) * 100 /
+						parseFloat(getNumber(country[1]))).toFixed(2))}%`,
+					recovered: getInnerText(country[5]),
+					active: getInnerText(country[6]),
+					critical: getNumber(country[7]),
+				} : undefined
+			}
+		}, countryName);
+
+		// printing
+		console.table({ cases: result.cases, deaths: result.deaths, deathRate: result.deathRate });
+		result.country ? console.table(result.country) : console.log();
+
+		await browser.close();
+	}
+	catch (e) {
+		console.error(e.message);
+	}
+}
+getCountryDetails();
