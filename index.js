18d28d072ed5c02d6687d54cd29ba1696136c4e2
skylerdj <jeffreyjoumjian@hotmail.com>
2020-05-23 11:24:51 +0300

    - fixed code to iterate over less table elements added indices object to make sure the indices are calculated dynamically (new columns don't mess up indices)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function getCountryDetails() {
 	// countries may return wrong values for new cases if the website hasn't set them properly
 	const countryName = process.argv[2] ? process.argv.slice(2).join(" ").toLowerCase().trim() : undefined;
 
-	const browser = await puppeteer.launch({ executablePath });
+	const browser = await puppeteer.launch({ executablePath, headless: true });
 	let page;
 
 	try {
@@ -30,60 +30,102 @@ async function getCountryDetails() {
 			function getInnerText(element) {
 				return element.innerText ? element.innerText : 0;
 			}
-			function stringify(objectArray) {
+			function stringify(...objectArray) {
 				// converts all the properties of an object to type String
 				objectArray.forEach(obj => {
 					Object.keys(obj).forEach(o => obj[o] = obj[o].toLocaleString());
 				});
 			}
+			function getIndices(tableHead) {
+				const indices = {};
+				tableHead.childNodes.forEach((col, ind) => {
+					switch (col.innerText.trim().toLowerCase().split('\n').join(" ")) {
+						case 'country, other': indices.country = ind;
+							break;
+						case 'total cases': indices.total = ind;
+							break;
+						case 'new cases': indices.newCases = ind;
+							break;
+						case 'total deaths': indices.deaths = ind;
+							break;
+						case 'new deaths': indices.newDeaths = ind;
+							break;
+						case 'total recovered': indices.recovered = ind;
+							break;
+						case 'active cases': indices.active = ind;
+							break;
+						case 'serious, critical': indices.critical = ind;
+							break;
+						case 'total tests': indices.tests = ind;
+							break;
+						default: break;
+					}
+				});
+				return indices;
+			}
 
-			const numbers = [...document.querySelectorAll('.maincounter-number > span')];
-			const table = [...document.querySelectorAll('tbody tr td')];
-			const overall = table.filter(e => e.innerText.toLowerCase() === 'world')[0].parentNode.children;
+			function getCountryDetails(country, indices) {
+				return {
+					country: getInnerText(country[indices.country]),
+					total: getInnerText(country[indices.total]),
+					newCases: getNumber(country[indices.newCases]),
+					deaths: getNumber(country[indices.deaths]),
+					newDeaths: getNumber(country[indices.newDeaths]),
+					active: getInnerText(country[indices.active]),
+					recovered: getInnerText(country[indices.recovered]),
+					critical: getNumber(country[indices.critical]),
+					deathRate: `${parseFloat((getNumber(country[indices.deaths]) * 100 /
+						parseFloat(getNumber(country[indices.total]))).toFixed(2))}%`,
+					realDeathRate: `${parseFloat((getNumber(country[indices.deaths]) * 100 /
+						parseFloat(getNumber(country[indices.total]) - getNumber(country[indices.active]))).toFixed(2))}%`,
+					tests: getInnerText(country[indices.tests])
+				}
+			}
+
+			// const numbers = [...document.querySelectorAll('.maincounter-number > span')];
+			let table = document.querySelector('table');
+			const overall = table.querySelector('.total_row_world').children;
+
+			table = {
+				tableHead: table.querySelector('thead tr'),
+				tableBody: [...table.querySelectorAll('tbody tr')]
+			};
+
+			const indices = getIndices(table.tableHead);
+
+
 
 			// general numbers about the virus
 			const general = {
-				total: getNumber(numbers[0]),
-				newCases: getNumber(overall[2]),
-				deaths: getNumber(numbers[1]),
-				newDeaths: getNumber(overall[4])
+				total: getNumber(overall[indices.total]),
+				active: getNumber(overall[indices.active]),
+				newCases: getNumber(overall[indices.newCases]),
+				deaths: getNumber(overall[indices.deaths]),
+				newDeaths: getNumber(overall[indices.newDeaths])
 			}
+
 			general.deathRate = `${parseFloat((general.deaths * 100 / general.total).toFixed(2))}%`
 
-			const detailed = {
-				active: general.total - general.deaths - getNumber(numbers[2]),
-				recovered: getNumber(numbers[2])
-			}
-			detailed.closed = general.total - detailed.active;
+			const detailed = {};
+			detailed.closed = general.total - general.active;
+			detailed.recovered = getNumber(overall[indices.recovered]);
 			detailed.realDeathRate = `${parseFloat((general.deaths * 100 / detailed.closed).toFixed(2))}%`
 
 
 			let country;
 			// if country is provided => get country row
 			if (countryName) {
-				try { country = table.filter(e => e.innerText.toLowerCase() === countryName)[0].parentNode.children; }
+				try { country = table.tableBody.find(e => e.children[1].innerText.trim().toLowerCase() === countryName).children; }
 				catch (e) { } // note can't do anything here since it would be in the browser and not the process
 			}
 
 			// stringify before returning
-			stringify([general, detailed]);
+			stringify(general, detailed);
 
 			return {
 				general, detailed,
 				// if country is provided => add country property with details
-				country: country ? {
-					country: getInnerText(country[0]),
-					total: getInnerText(country[1]),
-					newCases: getNumber(country[2]),
-					deaths: getNumber(country[3]),
-					newDeaths: getNumber(country[4]),
-					deathRate: `${parseFloat((getNumber(country[3]) * 100 /
-						parseFloat(getNumber(country[1]))).toFixed(2))}%`,
-					recovered: getInnerText(country[5]),
-					active: getInnerText(country[6]),
-					critical: getNumber(country[7]),
-					tests: getInnerText(country[10])
-				} : undefined
+				country: country ? getCountryDetails(country, indices) : undefined
 			}
 		}, countryName);
 
